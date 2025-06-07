e6eb7334cd6de64d130f69f6a0e7b8656f09e0bc
José Vinicius <sjosevinicius254@gmail.com>
2025-06-07 19:13:12 -0300

    Criando Get Request

--- App/index.js
+++ App/index.js
@@ -10,3 +10,12 @@ muda a porta do servidor
 const app = express()
 const bc = new Blockchain()
 
+// requisição de toda a cadeeia de blocos
+app.get('/blocks', (req, res) =>{
+    res.json(bc.chain)
+});
+
+// escutando na determinada HTTP_PORT
+app.listen(HTTP_PORT, () => {
+    console.log(`Listening on port ${HTTP_PORT}`)
+})
