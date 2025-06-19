4b48f33ca78a6982eef1290a2c7e3a1e5ed21311
José Vinicius <sjosevinicius254@gmail.com>
2025-06-18 21:11:48 -0300

    Criação do metodo id para colocar os ids nas transações e criação da classe Transaction

--- wallet/index.js
+++ wallet/index.js
@@ -14,6 +14,8 @@ class Wallet {
       balance  : ${this.balance}
     `;
   }
+
+  
 }
 
 module.exports = Wallet;
---
+++ wallet/transaction.js
@@ -0,0 +1,13 @@
+const ChainUtil = require("../chain-util");
+
+class Transaction {
+  constructor() {
+    this.id = ChainUtil.id;
+    this.input = null;
+    this.outputs = [];
+  }
+
+  static newTransaction(){
+    
+  }
+}
